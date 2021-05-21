356884d1379c78fb31aab21182c6b0bc4ba6c05e
tim hall <timhall@tims-mbp.lan>
2021-05-21 14:38:26 -0500

    fixed naming conflicts

--- database/migrations/2021_05_12_114400_create_yotpo_users_table.php
+++ database/migrations/2021_05_12_114400_create_yotpo_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use EolabsIo\YotpoApi\Domain\Shared\Migrations\YotpoMigration;
 
-class CreateUsersTable extends YotpoMigration
+class CreateYotpoUsersTable extends YotpoMigration
 {
     /**
      * Run the migrations.
